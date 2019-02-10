1e305e7b0f595adfee712255a1c8a103eadd5442
nawatend <32384185+nawatend@users.noreply.github.com>
2019-02-10 17:47:19 +0100

    entry one

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let btnViewProjects = document.getElementById('btnViewProjects');
+
+let start = document.getElementById('start');
+
+
+btnViewProjects.addEventListener('click', () => {
+    console.log('view projects clicked');
+    
+
+    
+
+});
