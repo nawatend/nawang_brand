ca8021827e534e9f01f73bfceca201f287830258
nawatend <32384185+nawatend@users.noreply.github.com>
2019-05-23 16:44:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 let btnViewProjects = document.getElementById('btnViewProjects');
 
-let start = document.getElementById('start');
-// let darkMode = document.getElementsByClassName('dark__mode')[0];
-
-
-// darkMode.addEventListener('click', () => {
-
-//     darkMode.classList.toggle('dark__mode--on')
-// })
 
 btnViewProjects.addEventListener('click', () => {
     console.log('view projects clicked');
