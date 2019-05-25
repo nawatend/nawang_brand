815597cd8c5041a58b87c87574cfa4ca95a4c10f
nawatend <32384185+nawatend@users.noreply.github.com>
2019-05-25 14:52:50 +0200

    scroll reveal added

--- js/main.js
+++ js/main.js
@@ -5,3 +5,41 @@ btnViewProjects.addEventListener('click', () => {
     console.log('view projects clicked');
 
 });
+
+
+ScrollReveal().reveal('.project', {
+    delay: 80
+});
+
+ScrollReveal().reveal('.skill', {
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.intro__text', {
+    delay: 100
+});
+
+
+ScrollReveal().reveal('.intro__viewprojects', {
+    delay: 200
+});
+
+ScrollReveal().reveal('.download__cv', {
+    delay: 300
+});
+
+ScrollReveal().reveal('#intro_svg', {
+    delay: 200
+});
+
+
+ScrollReveal().reveal('#performace', {
+    delay: 200
+});
+ScrollReveal().reveal('#innovation', {
+    delay: 300
+});
+ScrollReveal().reveal('#responsive', {
+    delay: 400
+});
